a37f538d38e0091542a76ed77085c72e0076e847
DESKTOP-FBU4NP8\에드 <qiew@naver.com>
2025-11-22 01:39:40 +0900

    1시 38분 테스트 -  edstyle7 - 이클립스   - 브랜치

--- src/pages/index/components/Loading.tsx
+++ src/pages/index/components/Loading.tsx
@@ -5,7 +5,7 @@ import styles from './Loading.module.scss'
 // 1시 10분 테스트 -  edstyle7 - 이클립스   - 브랜치     
 // 1시 20분 테스트 -  edstyle7 - 이클립스   - 브랜치    
 // 1시 30분 테스트 -  edstyle7 - 이클립스   - 브랜치 
-
+// 1시 38분 테스트 -  edstyle7 - 이클립스   - 브랜치 
 
 export default function Loading() {
   return (
