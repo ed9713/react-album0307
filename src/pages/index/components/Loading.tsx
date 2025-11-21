eed7f78cb123e86e379a128521a8e06c30ccd03d
edstyle7 <qiew@naver.com>
2025-11-22 01:12:55 +0900

    1시 10분 테스트 -  edstyle7 - 이클립스   - 브랜치

--- src/pages/index/components/Loading.tsx
+++ src/pages/index/components/Loading.tsx
@@ -2,6 +2,8 @@ import styles from './Loading.module.scss'
 
 
 // 12시 40분 테스트 -  edstyle7 - 이클립스   - 브랜치     
+// 1시 10분 테스트 -  edstyle7 - 이클립스   - 브랜치     
+
 
 export default function Loading() {
   return (
